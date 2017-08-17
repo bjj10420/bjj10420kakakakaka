31632b89cdbd1e60717a567b54361a7b5472602d
bjj <bjj10420@hotmail.com>
2017-08-17 15:38:28 +0900

    order값을 붙여주기 전

--- app/src/main/java/com/example/schedulemanager/DBHelper.java
+++ app/src/main/java/com/example/schedulemanager/DBHelper.java
@@ -157,6 +157,8 @@ public class DBHelper extends SQLiteOpenHelper {
         }
 //        Log.d("addScheduleToMapByMonth" , String.valueOf(Integer.parseInt(schedule.getDate().substring(6,8))));
         scheduleMap.put(Integer.parseInt(schedule.getDate().substring(6,8)), schedule);
+        Log.d("addScheduleToMapByMonth put 테스트" , schedule.getDate().substring(6,8));
     }
+
 
 }
