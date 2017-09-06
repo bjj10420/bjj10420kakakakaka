46f3b7300bd939a64dc087c6b863cb7346df1d8e
bjj <bjj10420@hotmail.com>
2017-09-06 15:35:17 +0900

    페이징 어댑터 수정 전 백업

--- app/src/main/java/com/example/schedulemanager/activity/MainActivity.java
+++ app/src/main/java/com/example/schedulemanager/activity/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+       //
     }
 
     private void init() {
