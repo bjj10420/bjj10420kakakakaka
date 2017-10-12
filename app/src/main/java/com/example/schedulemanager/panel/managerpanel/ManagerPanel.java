494e0a3077a34abaf2abe06e525f2845146ace22
jaejin <bjj10420@hotmail.com>
2017-10-12 12:55:56 +0900

    매니져 패널 레이아웃 설정 완료

--- app/src/main/java/com/example/schedulemanager/panel/managerpanel/ManagerPanel.java
+++ app/src/main/java/com/example/schedulemanager/panel/managerpanel/ManagerPanel.java
@@ -1,5 +1,21 @@
 package com.example.schedulemanager.panel.managerpanel;
 
+import android.content.Context;
+
 // 기타버튼 => 관리 화면
 public class ManagerPanel {
+
+    Context context;
+
+    public ManagerPanel(Context context) {
+        init(context);
+    }
+
+    public void init(Context context) {
+        this.context = context;
+    }
+
+    public void initPanel(){
+
+    }
 }
