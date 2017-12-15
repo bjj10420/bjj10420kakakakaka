595b51d0101b6b0f7229c7d16903f3b4790ed085
jaejin <bjj10420@hotmail.com>
2017-12-15 15:16:07 +0900

    1215 백업 커밋

--- app/src/main/java/com/example/schedulemanager/activity/MainActivity.java
+++ app/src/main/java/com/example/schedulemanager/activity/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mainActivity = this;
         init();
+       //
     }
 
     private void init() {
